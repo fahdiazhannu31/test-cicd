0624b8d81e2648763a37f65d3d88c4c3700dcdca
skyfallzn <skyfalltgr@gmail.com>
2024-09-17 14:25:51 +0700

    Upload file

--- src/Palindrome.php
+++ src/Palindrome.php
@@ -5,7 +5,7 @@ class Palindrome
 
 	public static function isPalindrome(string $str): bool
 	 {
-		$cleanedStr = strlower(preg_replace('/\W|_/', '', $str));
+		$cleanedStr = strtolower(preg_replace('/\W|_/', '', $str));
 		$reversedStr = strrev($cleanedStr);
 
 		return $cleanedStr === $reversedStr;
